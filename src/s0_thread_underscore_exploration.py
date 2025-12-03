0e32ed7f3615589356923da240d5de54a0bc92bd
Matt <yeamusic21@yahoo.com>
2025-12-03 07:55:45 -0500

    file renaming and added threading file

--- src/s0_thread_underscore_exploration.py
+++
@@ -1,9 +0,0 @@
-import _thread
-import time
-
-def print_epoch(nameOfThread, delay):
-    count = 0
-    while count < 3:
-        time.sleep(delay)
-        count += 1
-        print(nameOfThread, "------", time.time())
