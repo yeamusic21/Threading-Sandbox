5330ff10769b5ac38f86ad6fcb6500a3ebcb8768
Matt <yeamusic21@yahoo.com>
2025-12-08 16:14:51 -0500

    started reviewing threading

--- src/s0p1_threading_exploration.py
+++ src/s0p1_threading_exploration.py
@@ -1,2 +1,23 @@
 import threading 
 import time 
+
+
+def print_epoch(nameOfThread, delay):
+    count = 0
+    while count < 3:
+        time.sleep(delay)
+        count += 1
+        print(nameOfThread, "------", time.time())
+
+if __name__=="__main__":
+    # create threads
+    t1 = threading.Thread(target=print_epoch,args=("thread 1",1))
+    t2 = threading.Thread(target=print_epoch,args=("thread 2",2))
+    # start threads
+    t1.start()
+    t2.start()
+    # wait until threads are done
+    t1.join()
+    t2.join()
+    # print done!
+    print("All done!")
