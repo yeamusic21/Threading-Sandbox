cb1e841d261faba7ca050c0d147451779016401a
Matt <yeamusic21@yahoo.com>
2025-12-10 17:17:14 -0500

    added print cube and square functions

--- src/s0p1_threading_exploration.py
+++ src/s0p1_threading_exploration.py
@@ -9,10 +9,20 @@ def print_epoch(nameOfThread, delay):
         count += 1
         print(nameOfThread, "------", time.time())
 
+
+def print_cube(x):
+    print(f"Cube is {x*x*x}")
+
+def print_square(x):
+    print(f"Square is {x*x}")
+
+
 if __name__=="__main__":
     # create threads
-    t1 = threading.Thread(target=print_epoch,args=("thread 1",1))
-    t2 = threading.Thread(target=print_epoch,args=("thread 2",2))
+    # t1 = threading.Thread(target=print_epoch,args=("thread 1",1))
+    # t2 = threading.Thread(target=print_epoch,args=("thread 2",2))
+    t1 = threading.Thread(target=print_cube,args=(2,))
+    t2 = threading.Thread(target=print_square,args=(2,))
     # start threads
     t1.start()
     t2.start()
