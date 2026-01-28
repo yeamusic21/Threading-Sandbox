99fb3d526ab0b592091cb25d99724324a0768a54
Matt <yeamusic21@yahoo.com>
2026-01-28 16:02:31 -0500

    more threading review

--- src/s0p1_threading_exploration.py
+++ src/s0p1_threading_exploration.py
@@ -1,6 +1,7 @@
 import threading 
 import time 
 
+### PART 1 ####
 
 def print_epoch(nameOfThread, delay):
     count = 0
@@ -16,17 +17,43 @@ def print_cube(x):
 def print_square(x):
     print(f"Square is {x*x}")
 
+### PART 2 ###
+
+class MyThread(threading.Thread):
+    def __init__(self, name, delay):
+        threading.Thread.__init__(self)
+        self.name = name
+        self.delay = delay
+
+    def run(self):
+        print(f"start {self.name} thread")
+        print_epoch(self.name, self.delay)
+        print(f"end {self.name} thread")
+
 
 if __name__=="__main__":
-    # create threads
-    # t1 = threading.Thread(target=print_epoch,args=("thread 1",1))
-    # t2 = threading.Thread(target=print_epoch,args=("thread 2",2))
-    t1 = threading.Thread(target=print_cube,args=(2,))
-    t2 = threading.Thread(target=print_square,args=(2,))
-    # start threads
+    # ### PART 1 ###
+    # # create threads
+    # # t1 = threading.Thread(target=print_epoch,args=("thread 1",1))
+    # # t2 = threading.Thread(target=print_epoch,args=("thread 2",2))
+    # t1 = threading.Thread(target=print_cube,args=(2,))
+    # t2 = threading.Thread(target=print_square,args=(2,))
+    # # start threads
+    # t1.start()
+    # t2.start()
+    # # wait until threads are done
+    # t1.join()
+    # t2.join()
+    ### PART 2 ###
+    t1 = MyThread("thread-1", 1)
+    t2 = MyThread("thread-2", 2)
     t1.start()
     t2.start()
-    # wait until threads are done
+    print(t1.name)
+    print(t2.name)
+    print(threading.active_count())
+    print(threading.current_thread())
+    print(threading.enumerate())
     t1.join()
     t2.join()
     # print done!
